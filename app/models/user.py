c6599c3ff51a777b9f49d4d8ad2f20700009325d
Kyle Kassen <kyle.kassen@gmail.com>
2022-10-25 12:32:38 -0500

    users model updated

--- app/models/user.py
+++ app/models/user.py
@@ -7,9 +7,12 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(40), nullable=False)
+    lastname = db.Column(db.String(40), nullable=False)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    profile_picture = db.Column(db.String, default="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png")
 
     @property
     def password(self):
@@ -25,6 +28,9 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
+            'firstName': self.firstname,
+            'lastName': self.lastname,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'picture': self.profile_picture
         }
