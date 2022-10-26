cc13f8f713a71c7954deabbb11786c4087a44d86
Kyle Kassen <kyle.kassen@gmail.com>
2022-10-26 11:43:56 -0500

    create product route created

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,14 +1,18 @@
-from flask import Blueprint, jsonify
-from flask_login import login_required
-from app.models import Product, ProductImage, Review, ReviewImage, User
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+from app.models import Product, ProductImage, Review, ReviewImage, User, db
 from sqlalchemy import func
 from sqlalchemy.orm import joinedload, subqueryload
+from ..forms.add_product_form import AddProductForm
 
 product_routes = Blueprint('products', __name__)
 
-# Get Products
 @product_routes.route('/')
 def get_products():
+    """
+    Get All Products
+    """
+
     products = Product.query.options(joinedload(Product.product_image)).all()
     #products = Product.query.join(ProductImage).all()
 
@@ -22,12 +26,19 @@ def get_products():
     # return {'products': [image.to_dict() for product in products for image in product.product_image]}
     return result
 
-# Get One Product
 @product_routes.route('/<int:id>')
 def get_product(id):
+    """
+    Get One Product
+    """
+
     product = Product.query.get(id)
     images = ProductImage.query.filter_by(product_id=id).all()
     user = User.query.filter_by(id=product.seller_id).first()
+
+    rating = Review.query.with_entities(func.avg(Review.rating)).filter_by(product_id=id).first()
+    review_count = Review.query.count()
+    rating = float(round(rating[0], 1))
 
     # reviews = Review.query.filter_by(product_id=id).options(joinedload(Review.review_image)).all()
 
@@ -38,10 +49,50 @@ def get_product(id):
     #     result_reviews.append(current)
     # result['reviews'] = result_reviews
 
-
-
     result = product.to_dict()
     result['images'] = [image.to_dict() for image in images]
     result['seller'] = user.to_dict()
+    result['rating'] = rating
+    result['reviewCount'] = review_count
 
     return result
+
+@product_routes.route('/', methods=['POST'])
+@login_required
+def create_product():
+    """
+    Create a Product
+    """
+    user = current_user.to_dict()
+    seller_id = user['id']
+
+    form = AddProductForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        product = Product(
+            seller_id,
+            title=form.data['title'],
+            description=form.data['description'],
+            sold_by=form.data['sold_by'],
+            fulfilled_by=form.data['fulfilled_by'],
+            quantity=form.data['quantity'],
+            price=form.data['price'],
+            sale_price=form.data['sale_price'],
+            shipping_price=form.data['shipping_price'],
+            prime=form.data['prime']
+        )
+        db.session.add(product)
+        db.session.commit()
+
+        return product.to_dict()
+
+    return {'errors': 'an error occured'}
+
+@product_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_product():
+    """
+    Delete a Product
+    """
+    
---
+++ app/forms/add_product_form.py
@@ -0,0 +1,17 @@
+from tokenize import String
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FloatField, BooleanField
+from wtforms.validators import DataRequired
+
+class AddProductForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    sold_by = StringField('Sold By', validators=[DataRequired()])
+    fulfilled_by = StringField('Fulfilled By', validators=[DataRequired()])
+    quantity = StringField('Quantity', validators=[DataRequired()])
+    price = FloatField('Price', validators=[DataRequired()])
+    sale_price = FloatField('Sale Price', validators=[DataRequired()])
+    shipping_Price = FloatField('Shipping Price', validators=[DataRequired()])
+    prime = BooleanField('Prime', validators=[DataRequired()])
+
+    submit = SubmitField('Submit')
