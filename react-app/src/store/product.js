4c862ad825384d2c424b652fdc20e1d6d62ea4b3
Kyle Kassen <kyle.kassen@gmail.com>
2022-10-26 14:50:24 -0500

    redux store created for products

---
+++ react-app/src/store/product.js
@@ -0,0 +1,170 @@
+const LOAD = "product/load";
+const ADD = "product/add";
+const UPDATE = "product/update";
+const DELETE = "product/delete";
+const LOAD_ALL = "product/loadAll";
+
+//##########################
+// LOAD ONE PRODUCT
+//##########################
+
+export const loadOne = (product) => {
+  console.log("Loading One Product");
+  return {
+    type: LOAD,
+    payload: product,
+  };
+};
+
+export const loadOneProduct = (id) => async (dispatch) => {
+  console.log("Loading One Product Thunk");
+  const response = await fetch(`/api/products/${id}/`);
+
+  const product = await response.json();
+  if (response.ok) {
+    dispatch(loadOne(product));
+  }
+  return product;
+};
+
+//##########################
+// ADD PRODUCT
+//##########################
+
+export const addOne = (product) => {
+  console.log("Adding One Product");
+  return {
+    type: ADD,
+    payload: product,
+  };
+};
+
+export const addOneProduct = (product) => async (dispatch) => {
+  console.log("Adding One Product Thunk");
+  const response = await fetch(`/api/reviews/`, {
+    method: "POST",
+    header: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(product),
+  });
+
+  const product = await response.json();
+
+  if (response.ok) {
+    dispatch(addOne(product));
+  }
+  return product;
+};
+
+//##########################
+// UPDATE PRODUCT
+//##########################
+
+export const updateOne = (product) => {
+  console.log("Updating One Product");
+  return {
+    type: UPDATE,
+    payload: product,
+  };
+};
+
+export const updateOneProduct = (product, id) => async (dispatch) => {
+  console.log("Updating One Product Thunk");
+  const response = await fetch(`/api/reviews/${id}`, {
+    method: "PUT",
+    header: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(product),
+  });
+
+  const product = await response.json();
+
+  if (response.ok) {
+    dispatch(updateOne(product));
+  }
+  return product;
+};
+
+//##########################
+// DELETE PRODUCT
+//##########################
+
+export const deleteOne = () => {
+  console.log("Deleting One Product");
+  return {
+    type: DELETE,
+    payload: product,
+  };
+};
+
+export const deleteOneProduct = (id) => async (dispatch) => {
+  console.log("Deleting One Product Thunk");
+  const response = await fetch(`/api/products/${id}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    dispatch(deleteOne());
+  }
+  return await response.json();
+};
+
+//##########################
+// LOAD ALL PRODUCTS
+//##########################
+
+export const loadAll = (products) => {
+  console.log("Loading all Product");
+  return {
+    type: LOAD_ALL,
+    payload: products,
+  };
+};
+
+export const loadAllProducts = () => async (dispatch) => {
+  console.log("Loading all Product Thunk");
+  const response = await fetch(`/api/products/`);
+
+  products = await response.json();
+
+  if (response.ok) {
+    dispatch(loadAll(products));
+  }
+
+  return products;
+};
+
+//##########################
+// Reducer
+//##########################
+const initialState = { allProducts: {}, singleProduct: {} };
+
+export const productReducer = (state = initialState, action) => {
+  let newState = {
+    ...state,
+    allProducts: { ...state.allProducts },
+    singleProduct: { ...state.singleProduct },
+  };
+  switch (action.type) {
+    case LOAD:
+      newState.singleProduct = action.payload;
+      return newState;
+    case ADD:
+      newState.singleProduct = action.payload;
+      return newState;
+    case UPDATE:
+      newState.singleProduct = action.paylaod;
+      return newState;
+    case DELETE:
+      delete newState.allProducts[action.payload];
+      return newState;
+    case LOAD_ALL:
+        newState.allProducts = {...action.payload}
+        return newState
+
+    default:
+      return state;
+  }
+};
