eddde416226afbc01ebe0398bfee35ed756832ea
Kyle Kassen <kyle.kassen@gmail.com>
2022-10-27 15:35:18 -0500

    home carousel working

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -6,26 +6,52 @@ import home3 from "../../media/images/home3.jpg";
 import "./homepage.css";
 
 function Homepage() {
-    const [imageClass, setImageClass] = useState(1);
+  const [imageIndex, setImageIndex] = useState(1);
+  const [image1, setImage1] = useState("1");
+  const [image2, setImage2] = useState("");
+  const [image3, setImage3] = useState("");
 
-    let changeSlide = incr => {
+  let changeSlide = (incr) => {
 
-    }
+      if ((imageIndex===3 && incr===1)||(imageIndex===2 && incr===-1)) setImage1("1");
+      else setImage1("")
+      if ((imageIndex===1 && incr===1)||(imageIndex===3 && incr===-1)) setImage2("2");
+      else setImage2("")
+      if ((imageIndex===2 && incr===1)||(imageIndex===1 && incr===-1)) setImage3("3");
+      else setImage3("")
+
+      if (imageIndex === 1 && incr === -1) setImageIndex(3);
+      else if (imageIndex === 3 && incr === 1) setImageIndex(1);
+      else setImageIndex(imageIndex + incr);
+  };
   return (
     <>
       <div className="home-outer-wrapper">
         <div className="home-outer-container">
           <div className="home-carousel-container">
-            <a className="home-carousel-previous" onclick={()=> changeSlide(-1)}>
-            &#10094;
+            <a
+              className="home-carousel-previous"
+              tabindex="0"
+              onClick={() => changeSlide(-1)}
+            >
+              &#10094;
             </a>
-            <a className="home-carousel-next" onclick={()=> changeSlide(-1)}>
-            &#10095;
+            <a className="home-carousel-next" tabindex="0" onClick={() => changeSlide(1)}>
+              &#10095;
             </a>
             <div className="home-carousel-image-container">
-                <img className={`home-carousel-image${imageClass}`}src={home1} />
-                <img className={`home-carousel-image${imageClass}`}src={home2} />
-                <img className={`home-carousel-image${imageClass}`}src={home3} />
+              {/* {imageClass === 1 && (
+                <img className={`home-carousel-image-show`} src={home1} />
+              )}
+              {imageClass === 2 && (
+                <img className={`home-carousel-image-show`} src={home2} />
+              )}
+              {imageClass === 3 && (
+                <img className={`home-carousel-image-show`} src={home3} />
+              )} */}
+                <img className={`home-carousel-image${image1}`} src={home1} />
+                <img className={`home-carousel-image${image2}`} src={home2} />
+                <img className={`home-carousel-image${image3}`} src={home3} />
             </div>
           </div>
           <h1>HomePage</h1>
