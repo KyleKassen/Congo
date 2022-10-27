757b5352567a9e9dd9c63018e6703d64268720eb
Kyle Kassen <kyle.kassen@gmail.com>
2022-10-27 15:51:24 -0500

    created new homepage

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -9,7 +9,21 @@ function Homepage() {
     <>
       <Carousel />
       <div className="home-outer-wrapper">
-        
+        <div className="home-outer-container">
+          <div className="home-grid-container">
+            <div className="home-grid-item-1">
+              <div className="home-grid-item-1-header">
+
+              </div>
+              <div className="home-grid-item-1-body">
+
+              </div>
+              <div className="home-grid-item-1-footer">
+                
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
