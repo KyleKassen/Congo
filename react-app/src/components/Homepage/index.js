dcce1009454ee7cc21b183e518921a9a2efd5b0f
Kyle Kassen <kyle.kassen@gmail.com>
2022-10-27 13:10:49 -0500

    homepage component created

---
+++ react-app/src/components/Homepage/index.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react";
+import {useSelector} from "react-redux";
+import "./homepage.css"
+
+function Homepage(){
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Homepage;
