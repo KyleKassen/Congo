1bb9cae0fe1ac007ac70b07d2730f625ac22e73a
Kyle Kassen <kyle.kassen@gmail.com>
2022-10-28 12:01:02 -0500

    seed data for products added

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -21,14 +21,45 @@ def seed_products():
         description ='The 4-Series Roku TV delivers stunning 4K picture quality with four times the resolution of Full HD for enhanced clarity and detail, as well as endless entertainment with thousands of streaming channels. High dynamic range (HDR) technology delivers bright and accurate colors for a lifelike viewing experience. In addition, your favorite HD shows, movies, and sporting events are upscaled to near Ultra HD resolution with 4K Upscaling. The simple, personalized home screen allows seamless access to thousands of streaming channels, plus your cable box, Blu-ray player, gaming console, and other devices without flipping through inputs or complicated menus. The super-simple remote—with about half the number of buttons on a traditional TV remote—puts you in control of your favorite entertainment.',
         fulfilled_by = 'Amazon.com',
         sold_by = 'Amazon.com',
+        quantity = 100,
+        price = 599.99,
+        sale_price = 319.99,
+        prime = True
+    )
+
+    p3 = Product(
+        seller_id=1,
+        title = 'TCL 32" Class 3-Series HD 720p LED Smart Roku TV - 32S355',
+        description ="The TCL 3-Series is the simply smart way to enjoy endless entertainment. Enjoy over half a million movies and TV episodes available to stream plus sports, news, music, kids and family, food, science and tech, fitness, foreign language and so much more. Start streaming free TV right from your home screen or browse 250+ free live channels with the Live TV Channel Guide to find what to watch next. Your favorite broadcast TV, streaming channels, gaming console and other devices are front-and-center within a simple, customizable home screen. There's no more flipping through inputs or wading through complicated menus. The super-simple remote—with about half the number of buttons on a traditional TV remote—puts you in control of your favorite entertainment and includes one-touch shortcuts to popular channels like Netflix.",
+        fulfilled_by = 'Amazon.com',
+        sold_by = 'Amazon.com',
+        quantity = 100,
+        price = 229.99,
+        sale_price = 139.99,
+        prime = True
+    )
+    p4 = Product(
+        seller_id=1,
+        title = 'Mini PC Windows 11 Pro, 8GB DDR4 128GB SSD Intel Celeron J4125 Mini Desktop Computer, Support 2.5-inch SSD, 2.4G+5.0G WiFi, 4K HDMI x2, Gigabit Ethernet, BT 4.2, Support Windows 10 Pro Micro PC',
+        description ="The TCL 3-Series is the simply smart way to enjoy endless entertainment. Enjoy over half a million movies and TV episodes available to stream plus sports, news, music, kids and family, food, science and tech, fitness, foreign language and so much more. Start streaming free TV right from your home screen or browse 250+ free live channels with the Live TV Channel Guide to find what to watch next. Your favorite broadcast TV, streaming channels, gaming console and other devices are front-and-center within a simple, customizable home screen. There's no more flipping through inputs or wading through complicated menus. The super-simple remote—with about half the number of buttons on a traditional TV remote—puts you in control of your favorite entertainment and includes one-touch shortcuts to popular channels like Netflix.",
+        fulfilled_by = 'Amazon.com',
+        sold_by = 'Trend-KAMRUI',
+        quantity = 10,
+        price = 265.95,
+        sale_price = 189.95,
+        prime = True
     )
 
     for cat in categories:
         curr_cat = Category(name=cat)
         # curr_cat.products.append(fire_stick)
         # db.session.add(Category(name=cat))
+        if cat == 'Electronics' or cat == 'Movies & TV':
+            p2.categories.append(curr_cat)
+            p3.categories.append(curr_cat)
+
         fire_stick.categories.append(curr_cat)
-    db.session.add(fire_stick)
+    db.session.add_all([fire_stick, p2, p3, p4])
     db.session.commit()
 
 
@@ -40,6 +71,18 @@ def seed_products():
     twopm = ProductImage(
         product_id=1,
         url='https://m.media-amazon.com/images/I/411y5UdVmvL._AC_SL1000_.jpg'
+    )
+    pm3 = ProductImage(
+        product_id=2,
+        url='https://m.media-amazon.com/images/I/71wJCiFPrfL._AC_SL1500_.jpg'
+    )
+    pm4 = ProductImage(
+        product_id=3,
+        url='https://m.media-amazon.com/images/I/71yi46Ali0L._AC_SL1500_.jpg'
+    )
+    pm5 = ProductImage(
+        product_id=4,
+        url='https://m.media-amazon.com/images/I/71BuQGT+eXL._AC_SL1500_.jpg'
     )
 
     q1 = Question(
@@ -57,7 +100,7 @@ def seed_products():
 
     # fire_stick.categories.append
     # db.session.add(fire_stick)
-    db.session.add_all([onepm, twopm])
+    db.session.add_all([onepm, twopm, pm3, pm4, pm5])
     db.session.commit()
     db.session.add(q1)
     db.session.add(a1)
