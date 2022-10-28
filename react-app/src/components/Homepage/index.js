167196d312690ece2004ea61d3ea6e8de88a1ab5
Kyle Kassen <kyle.kassen@gmail.com>
2022-10-28 12:41:48 -0500

    restructured home page

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -4,25 +4,35 @@ import { useSelector } from "react-redux";
 import "./homepage.css";
 
 function Homepage() {
+  let products = useSelector(state => Object.values(state.products.allProducts))
+
+  let saleProducts = []
+  for(let product in products) {
+    if(product.salePrice) {
+      saleProducts.push(product)
+    }
+  }
+
+
+  console.log(products[1].images[0].url)
 
   return (
     <>
       <Carousel />
       <div className="home-outer-wrapper">
         <div className="home-outer-container">
-          <div className="home-grid-container">
-            <div className="home-grid-item-1">
-              <div className="home-grid-item-1-header">
+          {saleProducts.map(product => {
+            return (
+              <div className="home-flex-item">
+                <h2>{product.title.slice(0,15)}</h2>
+              </div>
+            )
+          })}
 
-              </div>
-              <div className="home-grid-item-1-body">
 
-              </div>
-              <div className="home-grid-item-1-footer">
-                
-              </div>
-            </div>
-          </div>
+
+
+
         </div>
       </div>
     </>
