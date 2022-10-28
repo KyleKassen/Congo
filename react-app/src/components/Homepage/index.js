c296adf3d7059004020312e8a67e37aa60ce9a1a
Kyle Kassen <kyle.kassen@gmail.com>
2022-10-28 12:55:26 -0500

    basic structure create

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -1,38 +1,50 @@
 import React, { useState, useEffect } from "react";
-import Carousel from '../Carousel'
+import Carousel from "../Carousel";
 import { useSelector } from "react-redux";
 import "./homepage.css";
 
 function Homepage() {
-  let products = useSelector(state => Object.values(state.products.allProducts))
+  let products = useSelector((state) =>
+    Object.values(state.products.allProducts)
+  );
 
-  let saleProducts = []
-  for(let product in products) {
-    if(product.salePrice) {
-      saleProducts.push(product)
+  let saleProducts = [];
+  for (let product of products) {
+    // console.log(product)
+    if (product.salePrice) {
+      saleProducts.push(product);
     }
   }
 
-
-  console.log(products[1].images[0].url)
+  // console.log(products)
 
   return (
     <>
       <Carousel />
       <div className="home-outer-wrapper">
         <div className="home-outer-container">
-          {saleProducts.map(product => {
+          {saleProducts.map((product) => {
+            let salePercentage = (
+              1 -
+              product.salePrice / product.price
+            ).toString();
+            let formatPerc = salePercentage.slice(2, 4);
             return (
               <div className="home-flex-item">
-                <h2>{product.title.slice(0,15)}</h2>
+                <h2>Epic Deals</h2>
+                <div className="home-product-img-container">
+                  <img src={product.images[0].url} />
+                </div>
+                <p>
+                  {formatPerc}% off <span>Top deal</span>
+                </p>
+                <p>
+                  ${product.salePrice} List Price: {product.price}
+                </p>
+                <p>{product.title.slice(0, 45)} ...</p>
               </div>
-            )
+            );
           })}
-
-
-
-
-
         </div>
       </div>
     </>
