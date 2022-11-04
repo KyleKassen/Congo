9ffbfde83d5145fddc6473b6f238ff6b6b3088a5
Kyle Kassen <kyle.kassen@gmail.com>
2022-11-04 14:06:08 -0500

    additional signup validators

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -37,7 +37,10 @@ const SignUpForm = () => {
     }
 
     if (!username) currentErrors['name'] = 'Enter your name';
+    if (username.length > 59) currentErrors['name'] = 'Entered name is too long'
     if (!email) currentErrors['email'] = 'Enter your email';
+    if (email.length > 254) currentErrors['email'] = 'Entered email is too long';
+    if (password.length > 254) currentErrors['password'] = 'Maximum of 255 characters';
     if (password.length < 6) currentErrors['password'] = 'Minimum 6 characters required';
     if (password !== repeatPassword) currentErrors['password2'] = 'Passwords must match'
     if (!email.includes(".") || !email.includes("@")) currentErrors['email'] = 'Enter a valid email'
