4ebdb9bc955afb88d15cc9203444ff045cc491ce
Kyle Kassen <kyle.kassen@gmail.com>
2022-11-15 16:07:04 -0600

    shopping cart headers created

--- react-app/src/components/Cart/index.js
+++ react-app/src/components/Cart/index.js
@@ -1,25 +1,25 @@
-import React, {useEffect, useState} from 'react';
-import {useSelector} from 'react-redux';
-import "./cart.css"
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import "./cart.css";
 
 function Cart() {
-
-    return (
-        <>
-        <div className="cart-outer-wrapper">
-            <div className="cart-flex-container">
-                <div className="cart-left-right-container">
-                    <div className="cart-left-container">
-
-                    </div>
-                    <div className="cart-right-container">
-
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="cart-outer-wrapper">
+        <div className="cart-flex-container">
+          <div className="cart-left-right-container">
+            <div className="cart-left-container">
+              <h2>Shopping Cart</h2>
+              <p>Price</p>
             </div>
+            <div className="cart-right-container">
+              <p>right column</p>
+            </div>
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default Cart;
