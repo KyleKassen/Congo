39485170cb270db4be996865b82892bf08731a6f
Kyle Kassen <kyle.kassen@gmail.com>
2022-11-16 16:02:42 -0600

    custom quantity form created

--- react-app/src/components/Cart/index.js
+++ react-app/src/components/Cart/index.js
@@ -6,8 +6,30 @@ import "./cart.css";
 
 function Cart() {
   const history = useHistory();
+  const [quantities, setQuantities]= useState({});
+  const [currentItem, setCurrentItem] = useState(0);
 
   const cartItems = useSelector((state) => Object.values(state.cart.items));
+
+  const updateQuantity = async (itemId, quantity) => {};
+
+  const showCustomForm = (itemId) => {
+    const dropDownContainer = document.getElementById(`item-${itemId}dropdown`)
+    dropDownContainer.classList.add('disable-quantity-selection')
+  };
+
+  const handleChange = (itemId, e) => {
+    const currentQantities = quantities;
+    quantities[itemId] = e.target.value;
+
+    setQuantities({...currentQantities})
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    // console.log('first variable is :', e)
+    console.log('second variable is :', currentItem)
+  }
 
   return (
     <>
@@ -43,7 +65,7 @@ function Cart() {
                             delivery
                           </p>
                         </div>
-                        <div className="class-item-quantity-drop-down">
+                        <div className={`class-item-quantity-drop-down`} id={`item-${item.id}dropdown`}>
                           <div className="class-item-quantity-button">
                             <p>
                               Qty: <span>{item.quantity}</span> <i></i>
@@ -54,17 +76,37 @@ function Cart() {
                             {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => {
                               if (num === item.quantity && item.quantity < 10) {
                                 return (
-                                  <li className="class-item-quantity-active">
+                                  <li
+                                    className="class-item-quantity-active"
+                                    onClick={() => updateQuantity(item.id, num)}
+                                  >
                                     {num}
                                   </li>
                                 );
-                              } else if(num === 10 && item.quanity > 9) {
-                                return <li className="class-item-quantity-active">10+</li>
+                              } else if (num === 10 && item.quanity > 9) {
+                                return (
+                                  <li className="class-item-quantity-active class-item-10plus" onClick={() => showCustomForm(item.id)}>
+                                    10+
+                                  </li>
+                                );
+                              } else if (num === 10) {
+                                return <li className="class-item-10plus" onClick={() => showCustomForm(item.id)}>10+</li>;
                               } else {
-                                return <li>{num}</li>
+                                return <li>{num}</li>;
                               }
                             })}
                           </ul>
+                        </div>
+                        <div className={`class-item-custom-quantity`} id={`item-${item.id}custom`}>
+                            <form className="class-item-custom-form" onSubmit={handleSubmit}>
+                              <div className="custom-quantity-form-field">
+                                <input
+                                  type="number"
+                                  value={quantities[item.id]}
+                                  onChange={(e) => handleChange(item.id, e)}/>
+                              </div>
+                              <button type="submit" onClick={() => setCurrentItem(item.id)}>Update</button>
+                            </form>
                         </div>
                       </div>
                     </div>
