a03e39b82a90a9540a2e9b0a38e255a02803f565
Kyle Kassen <kyle.kassen@gmail.com>
2022-11-16 20:45:59 -0600

    fixed last cart item dropdown

--- react-app/src/components/Cart/index.js
+++ react-app/src/components/Cart/index.js
@@ -13,22 +13,33 @@ function Cart() {
   const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
-  const cartItems = Object.values(cart.items)
+  const cartItems = Object.values(cart.items);
   const userId = useSelector((state) => state.session.user.id);
 
   const updateQuantity = async (itemId, quantity) => {
     console.log(`quantity is : ${quantity}`);
-    const cartUlItems = document.getElementsByClassName("cart-item-ul")
-    console.log(cartUlItems)
-    Object.values(cartUlItems).forEach(ulItem => ulItem.classList.add("disable-hover"))
+    const cartUlItems = document.getElementsByClassName("cart-item-ul");
+    console.log(cartUlItems);
+    Object.values(cartUlItems).forEach((ulItem) =>
+      ulItem.classList.add("disable-hover")
+    );
     const response = await dispatch(editCartItem(itemId, quantity));
-    Object.values(cartUlItems).forEach(ulItem => ulItem.classList.remove("disable-hover"))
+    Object.values(cartUlItems).forEach((ulItem) =>
+      ulItem.classList.remove("disable-hover")
+    );
   };
+
+  useEffect(() => {
+    const lastItem = document.getElementById(`cart-ul-${cartItems.length - 1}`);
+    if (lastItem) lastItem.classList.add("cart-last-item-ul");
+  });
 
   const showCustomForm = (itemId) => {
     const dropDownContainer = document.getElementById(`item-${itemId}dropdown`);
     dropDownContainer.classList.add("disable-quantity-selection");
-    const customQuantityContainer = document.getElementById(`item-${itemId}custom`);
+    const customQuantityContainer = document.getElementById(
+      `item-${itemId}custom`
+    );
     customQuantityContainer.classList.add("show-custom-quantity");
   };
 
@@ -53,20 +64,17 @@ function Cart() {
     updateQuantity(currentItem, quantities[currentItem]);
 
     // Handle display styling for dropdown and field
-    const dropDownContainer = document.getElementById(`item-${currentItem}dropdown`);
-    const customQuantityContainer = document.getElementById(`item-${currentItem}custom`);
-    dropDownContainer.classList.remove('disable-quantity-selection')
-    customQuantityContainer.classList.remove('show-custom-quantity')
+    const dropDownContainer = document.getElementById(
+      `item-${currentItem}dropdown`
+    );
+    const customQuantityContainer = document.getElementById(
+      `item-${currentItem}custom`
+    );
+    dropDownContainer.classList.remove("disable-quantity-selection");
+    customQuantityContainer.classList.remove("show-custom-quantity");
   };
 
-  // Get rid of input field changing when scrolling
-  document.addEventListener("wheel", function (event) {
-    if (document.activeElement.type === "number") {
-      document.activeElement.blur();
-    }
-  });
-
-  let total = 0
+  let total = 0;
 
   return (
     <>
@@ -77,8 +85,8 @@ function Cart() {
               <h2>Shopping Cart</h2>
               <p>Price</p>
               <div className="cart-items-container">
-                {cartItems.map((item) => {
-                  total += item.quantity * item.product.price
+                {cartItems.map((item, idx) => {
+                  total += item.quantity * item.product.price;
                   return (
                     <div className="cart-item-container">
                       <div className="cart-item-left-container">
@@ -118,7 +126,10 @@ function Cart() {
                                   Qty: <span>{item.quantity}</span> <i></i>
                                 </p>
                               </div>
-                              <ul className="cart-item-ul">
+                              <ul
+                                className="cart-item-ul"
+                                id={`cart-ul-${idx}`}
+                              >
                                 <li
                                   onClick={() =>
                                     updateQuantity(item.product.id, 0)
@@ -217,13 +228,22 @@ function Cart() {
                         </div>
                       </div>
                       <div className="cart-item-right-container">
-                        <p className="cart-item-price-style">${item.product.price}</p>
+                        <p className="cart-item-price-style">
+                          ${item.product.price}
+                        </p>
                       </div>
                     </div>
                   );
                 })}
               </div>
-            <div className="class-item-price-container"><p className="class-items-subtotal">Subtotal ({cart.totalQuantity} items):<span className="cart-item-price-style">${Math.round(total * 100)/100}</span></p></div>
+              <div className="class-item-price-container">
+                <p className="class-items-subtotal">
+                  Subtotal ({cart.totalQuantity} items):
+                  <span className="cart-item-price-style">
+                    ${Math.round(total * 100) / 100}
+                  </span>
+                </p>
+              </div>
             </div>
             <div className="cart-right-container">
               <p>right column</p>
