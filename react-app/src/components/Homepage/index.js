e2db0a416c8f9611eeedbc760f6eb9a5ba7770f1
Kyle Kassen <kyle.kassen@gmail.com>
2022-11-18 08:19:57 -0600

    ratings added to homepage

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import Carousel from "../Carousel";
 import { useSelector } from "react-redux";
 import "./homepage.css";
@@ -21,6 +21,46 @@ function Homepage() {
 
   // console.log(products)
 
+  const getStars = (rating, addon) => {
+    return (
+      <>
+        {rating > 4.6 && (
+          <i className={`stars-img product-5-stars${addon}`}></i>
+        )}
+        {rating <= 4.6 && rating > 4 && (
+          <i className={`stars-img product-45-stars${addon}`}></i>
+        )}
+        {rating <= 4 && rating > 3.6 && (
+          <i className={`stars-img product-4-stars${addon}`}></i>
+        )}
+        {rating <= 3.6 && rating > 3 && (
+          <i className={`stars-img product-35-stars${addon}`}></i>
+        )}
+        {rating <= 3 && rating > 2.6 && (
+          <i className={`stars-img product-3-stars${addon}`}></i>
+        )}
+        {rating <= 2.6 && rating > 2 && (
+          <i className={`stars-img product-25-stars${addon}`}></i>
+        )}
+        {rating <= 2 && rating > 1.6 && (
+          <i className={`stars-img product-2-stars${addon}`}></i>
+        )}
+        {rating <= 1.6 && rating > 1 && (
+          <i className={`stars-img product-15-stars${addon}`}></i>
+        )}
+        {rating <= 1 && rating > 0.6 && (
+          <i className={`stars-img product-1-stars${addon}`}></i>
+        )}
+        {rating <= 0.6 && rating != null && (
+          <i className={`stars-img product-05-stars${addon}`}></i>
+        )}
+        {rating == null && (
+          <i className={`stars-img product-0-stars${addon}`}></i>
+        )}
+      </>
+    );
+  };
+
   return (
     <div className="homepage-wrapper">
       <Carousel />
@@ -33,7 +73,10 @@ function Homepage() {
             // ).toString();
             // let formatPerc = salePercentage.slice(2, 4);
             return (
-              <div className="home-flex-item" onClick={() => history.push(`/product/${product.id}`)}>
+              <div
+                className="home-flex-item"
+                onClick={() => history.push(`/product/${product.id}`)}
+              >
                 {/* <h2>Epic Deals</h2> */}
                 <div className="home-product-img-container">
                   <img src={product.images[0].url} />
@@ -44,8 +87,18 @@ function Homepage() {
                 {/* <p>
                   ${product.salePrice} List Price: {product.price}
                 </p> */}
-                {product.title.length > 80 && <p>{product.title.slice(0, 80)} ...</p>}
+                {product.title.length > 80 && (
+                  <p>{product.title.slice(0, 80)} ...</p>
+                )}
                 {product.title.length <= 80 && <p>{product.title}</p>}
+                <div className="product-rating-container">
+                  <div className="product-star-rating">
+                    {getStars(product.rating, "")}
+                  </div>
+                  <div className="product-rating-count">
+                    <span>{product.reviewCount} ratings</span>
+                  </div>
+                </div>
               </div>
             );
           })}
