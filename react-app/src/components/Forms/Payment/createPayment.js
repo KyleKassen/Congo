9041ca3882cf35b19529023b28d516c8a0e0edef
Kyle Kassen <kyle.kassen@gmail.com>
2022-11-29 15:25:09 -0600

    card images added

--- react-app/src/components/Forms/Payment/createPayment.js
+++ react-app/src/components/Forms/Payment/createPayment.js
@@ -109,109 +109,129 @@ function CreatePayment({
             ))}
           </div>
           <div className="form-middle-container">
-            <div className="input-container">
-              <p>Card number</p>
-              <input
-                id="form-field-cardnumber"
-                className="form-field"
-                // placeholder="Card Number"
-                type="text"
-                value={cardNumber}
-                onChange={(e) => setCardNumber(e.target.value)}
-                required
-              />
-            </div>
-            <div className="input-container">
-              <p>Name on card</p>
-              <input
-                id="form-field-cardholder"
-                className="form-field"
-                // placeholder="Card Holder"
-                type="text"
-                value={cardHolder}
-                onChange={(e) => setCardHolder(e.target.value)}
-                required
-              />
-            </div>
-            <div className="input-container">
-              <p>Expiration date</p>
-              <div
-                className="exp-month dropdown-button"
-                onClick={() => {
-                  if (!showMonths) setShowMonths(true);
-                }}
-                onMouseLeave={() => setShowMonths(false)}
-              >
-                <span>{cardMonth}</span>
-                <i className="icon-dropdown"></i>
-                {showMonths && (
-                  <div className="months-dropdown dropdown-container">
-                    <ul>
-                      {months.map((month, idx) => {
-                        return month == cardMonth ? (
-                          <li
-                            key={idx}
-                            className="active-list-item"
-                            onClick={() => handleMonthClick(month)}
-                          >
-                            {month}
-                          </li>
-                        ) : (
-                          <li key={idx} onClick={() => handleMonthClick(month)}>
-                            {month}
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </div>
-                )}
+            <div className="form-middle-left-container">
+              <div className="input-container">
+                <p>Card number</p>
+                <input
+                  id="form-field-cardnumber"
+                  className="form-field"
+                  // placeholder="Card Number"
+                  type="text"
+                  value={cardNumber}
+                  onChange={(e) => setCardNumber(e.target.value)}
+                  required
+                />
               </div>
-              <div
-                className="exp-year dropdown-button"
-                onClick={() => {
-                  if (!showYears) setShowYears(true);
-                }}
-                onMouseLeave={() => setShowYears(false)}
-              >
-                <span>{cardYear}</span>
-                <i className="icon-dropdown"></i>
-                {showYears && (
-                  <div className="years-dropdown dropdown-container">
-                    <ul>
-                      {years.map((year, idx) => {
-                        return year == cardYear ? (
-                          <li
-                            key={idx}
-                            className="active-list-item"
-                            onClick={() => handleYearClick(year)}
-                          >
-                            {year}
-                          </li>
-                        ) : (
-                          <li key={idx} onClick={() => handleYearClick(year)}>
-                            {year}
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </div>
-                )}
+              <div className="input-container">
+                <p>Name on card</p>
+                <input
+                  id="form-field-cardholder"
+                  className="form-field"
+                  // placeholder="Card Holder"
+                  type="text"
+                  value={cardHolder}
+                  onChange={(e) => setCardHolder(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="input-container">
+                <p>Expiration date</p>
+                <div
+                  className="exp-month dropdown-button"
+                  onClick={() => {
+                    if (!showMonths) setShowMonths(true);
+                  }}
+                  onMouseLeave={() => setShowMonths(false)}
+                >
+                  <span>{cardMonth}</span>
+                  <i className="icon-dropdown"></i>
+                  {showMonths && (
+                    <div className="months-dropdown dropdown-container">
+                      <ul>
+                        {months.map((month, idx) => {
+                          return month == cardMonth ? (
+                            <li
+                              key={idx}
+                              className="active-list-item"
+                              onClick={() => handleMonthClick(month)}
+                            >
+                              {month}
+                            </li>
+                          ) : (
+                            <li
+                              key={idx}
+                              onClick={() => handleMonthClick(month)}
+                            >
+                              {month}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                  )}
+                </div>
+                <div
+                  className="exp-year dropdown-button"
+                  onClick={() => {
+                    if (!showYears) setShowYears(true);
+                  }}
+                  onMouseLeave={() => setShowYears(false)}
+                >
+                  <span>{cardYear}</span>
+                  <i className="icon-dropdown"></i>
+                  {showYears && (
+                    <div className="years-dropdown dropdown-container">
+                      <ul>
+                        {years.map((year, idx) => {
+                          return year == cardYear ? (
+                            <li
+                              key={idx}
+                              className="active-list-item"
+                              onClick={() => handleYearClick(year)}
+                            >
+                              {year}
+                            </li>
+                          ) : (
+                            <li key={idx} onClick={() => handleYearClick(year)}>
+                              {year}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="input-container">
+                <p>
+                  <span>Security Code </span>
+                  <span>(CVV/CVC)</span>
+                </p>
+                <input
+                  id="form-field-securitycode"
+                  className="form-field"
+                  // placeholder="Security Code"
+                  type="number"
+                  value={securityCode}
+                  onChange={(e) => setSecurityCode(e.target.value)}
+                  required
+                />
               </div>
             </div>
-            <div className="input-container">
-              <p>
-                <span>Security Code </span>
-                <span>(CVV/CVC)</span>
-              </p>
-              <input
-                id="form-field-securitycode"
-                className="form-field"
-                // placeholder="Security Code"
-                type="number"
-                value={securityCode}
-                onChange={(e) => setSecurityCode(e.target.value)}
-                required
-              />
+            <div className="form-middle-right-container">
+              <p>Congo accepts all major debit cards:</p>
+              <i className="card1"></i>
+              <i className="card2"></i>
+              <i className="card3"></i>
+              <i className="card4"></i>
+              <i className="card5"></i>
+              <i className="card6"></i>
+              <i className="card7"></i>
+              <i className="card8"></i>
+              <i className="card9"></i>
+              <i className="card10"></i>
+              <i className="card11"></i>
+              <i className="card12"></i>
             </div>
           </div>
           <div className="form-bottom-buttons-container">
